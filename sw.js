09cb7adc83daa48b183d3bd2b4bfd528ce2e0774
shaulister <shauli@g-team.co.il>
2026-07-18 14:59:49 +0300

    עדכון מטמון האפליקציה

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="shauli-daily-v7";
-const ASSETS=["./","./index.html","./styles.css?v=2","./app.js?v=3","./manifest.webmanifest","./data/briefing.json"];
+const CACHE="shauli-daily-v8";
+const ASSETS=["./","./index.html","./styles.css?v=3","./app.js?v=4","./manifest.webmanifest","./data/briefing.json"];
 self.addEventListener("install",event=>event.waitUntil(Promise.all([caches.open(CACHE).then(cache=>cache.addAll(ASSETS)),self.skipWaiting()])));
 self.addEventListener("activate",event=>event.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))),self.clients.claim()])));
 self.addEventListener("fetch",event=>event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request))));
